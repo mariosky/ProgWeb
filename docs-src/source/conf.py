8e45ba6a3c41d2c2b46321c41c681052b87dbc7e
Mario Garcia-Valdez <mariosky@gmail.com>
2026-01-16 11:18:35 -0800

    ignore

--- docs-src/source/conf.py
+++ docs-src/source/conf.py
@@ -6,6 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+
+
 project = 'Programción Web'
 copyright = '2026, J Mario García Valdez'
 author = 'J Mario García Valdez'
@@ -14,15 +16,23 @@ release = '2026-1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinxcontrib.bibtex','sphinx_copybutton', ]
+copybutton_prompt_text = r">>>|\.\.\."
+copybutton_prompt_is_regexp = True
+bibtex_bibfiles = ['biblio.bib']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
-language = 'es-mx'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_title = 'Programción Web'
+
+exclude_patterns = []
+
+language = 'es-mx'
+
+
